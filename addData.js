38e0b42d29f9f599eb1d9ab6d645ac64444f630e
tungptseven <tungpt.seven@gmail.com>
2017-08-01 10:52:19 +0700

    fix data

--- addData.js
+++ addData.js
@@ -2,25 +2,29 @@
  * Created by msi on 07/04/2017.
  */
 var pgp = require('pg-promise')();
-var db = pgp('postgres://postgres:admin@163.44.206.220:5432/location');
-let data = require('./data.json');
-/*let json = {
-    1 : {
-        'name' : 'samsung galaxy 7',
-        'price' : 5700000
-    },
-    2 : {
-        'name' : 'samsung edge 7',
-        'price' : 6000000
-    }
-};*/
+var db = pgp('postgres://postgres:admin@163.44.206.220:5432/coffy');
+let data = require('./data (1).json');
+const myasync = require('async');
+
 
 for(let count in data){
-    db.any('insert into location values (${address}, ${district},${image},${lat},${long},${name},${octime},${rate},${type},${id})',data[count])
+    db.any('insert into coffy.image(name, id_location) values (${image}, ${id})',data[count])
         .then(()=>{
-        console.log('import succeed');
-})
-.catch(error => {
+            console.log('import succeed');
+        })
+        .catch(error => {
+            console.log('error',error);
+        });
+}
 
-    });
+
+
+for(let count in data){
+    db.any('insert into coffy.location(address, lat, long, name, octime, rate, id_location, id_district, id_type) values (${address},${lat},${long},${name},${octime},${rate},${id},${id_district},${id_type})',data[count])
+        .then(()=>{
+            console.log('import succeed');
+        })
+        .catch(error => {
+            console.log('error',error);
+        });
 }
